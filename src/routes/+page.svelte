1fd73fc89fb202c08a61c6733bdf4450fbfb2864
Cauê Farias Santos <caue.fsantos97@gmail.com>
2024-06-23 18:49:21 -0300

    feat: Adicionar componentes Cabecalho e MinhaLista
    Adiciona os componentes Cabecalho e MinhaLista ao projeto, que são responsáveis por exibir o cabeçalho e a lista de ingredientes personalizada do usuário.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
   import '../app.css';
+  import Cabecalho from '$lib/components/Cabecalho.svelte';
+  import MinhaLista from '$lib/components/MinhaLista.svelte';
 </script>
 
 <svelte:head>
   <title>Alura Cook</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container-principal">
+  <Cabecalho />
 
-<a href="/receitas">Ir para as receitas</a>
+  <div class="estilo-principal">
+    <div class="minha-lista-container">
+      <MinhaLista />
+    </div>
+  </div>
+</div>
 
-<img src="/imagens/receitas/panqueca.png" alt="Panqueca">
+<style>
+  .container-principal {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .estilo-principal {
+    text-align: center;
+    padding: 0 5vw 3.375rem;
+    flex: 1;
+  }
+
+  .minha-lista-container {
+    margin-bottom: 2rem;
+  }
+</style>
